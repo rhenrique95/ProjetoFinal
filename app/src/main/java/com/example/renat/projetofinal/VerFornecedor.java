ac407d267457038ab90c5c2af5e253fa193bc264
rhenrique95 <renatohenrique_95@hotmail.com>
2018-01-02 00:44:56 +0000

    Layouts

---
+++ app/src/main/java/com/example/renat/projetofinal/VerFornecedor.java
@@ -0,0 +1,14 @@
+package com.example.renat.projetofinal;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerFornecedor extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_fornecedor);
+    }
+}
