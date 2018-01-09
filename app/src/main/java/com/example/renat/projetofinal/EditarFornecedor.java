d6154123dab8344e6c8a957d2329a290f4857464
rhenrique95 <renatohenrique_95@hotmail.com>
2018-01-09 16:31:15 +0000

    Ecras Fornecedores e Layout v1

--- app/src/main/java/com/example/renat/projetofinal/EditarFornecedor.java
+++ app/src/main/java/com/example/renat/projetofinal/EditarFornecedor.java
@@ -1,14 +1,111 @@
 package com.example.renat.projetofinal;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditarFornecedor extends Activity {
+
+    protected Intent oIntent;
+    protected Button editar;
+    protected EditText nome;
+    protected EditText morada;
+    protected EditText contacto;
+    protected EditText email;
+    protected EditText empresa;
+    protected EditText notas;
+    protected AdaptadorBaseDados adb;
+    protected int _id;
+    protected Cursor cursor;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adb = new AdaptadorBaseDados(this).open();
+    }
+    protected void onPause() {
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adb.close();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_fornecedor);
+
+        nome = (EditText) findViewById(R.id.nome);
+        morada = (EditText) findViewById(R.id.morada);
+        contacto = (EditText) findViewById(R.id.contacto);
+        email = (EditText)findViewById(R.id.email);
+        empresa = (EditText) findViewById(R.id.empresa);
+        notas = (EditText) findViewById(R.id.notas);
+        editar = (Button) findViewById(R.id.editar);
+
+        adb = new AdaptadorBaseDados(this).open();
+
+        oIntent = getIntent();
+        _id = oIntent.getExtras().getInt("_id");
+
+        cursor = adb.obterFornecedorId(_id);
+
+        if(cursor.moveToFirst()){
+
+            nome.setText(cursor.getString(1));
+            morada.setText(cursor.getString(2));
+            contacto.setText(cursor.getString(3));
+            email.setText(cursor.getString(4));
+            empresa.setText(cursor.getString(5));
+            notas.setText(cursor.getString(6));
+        }
+
+
+
+        editar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if(nome.getText().toString().equals("") || morada.getText().toString().equals("") || contacto.getText().toString().equals("") || email.getText().toString().equals("") || empresa.getText().toString().equals("")){
+                    Toast toast = Toast.makeText(EditarFornecedor.this, "Todos os campos são obrigatórios!", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                else{
+                    Boolean res = adb.alteraFornecedor(_id, nome.getText().toString(), morada.getText().toString(), contacto.getText().toString(), email.getText().toString(), empresa.getText().toString(), notas.getText().toString());
+
+                    if (res) {
+                        Toast toast = Toast.makeText(EditarFornecedor.this, "Fornecedor guardado com sucesso!", Toast.LENGTH_LONG);
+                        toast.show();
+
+                        Intent intent=new Intent (EditarFornecedor.this, VerFornecedor.class);
+                        intent.putExtra("_id", _id);
+                        startActivity (intent);
+
+
+                    } else {
+                        Toast toast = Toast.makeText(EditarFornecedor.this, "Ocorreu um erro. Tente novamente!", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent=new Intent (EditarFornecedor.this,VerFornecedor.class);
+        intent.putExtra("_id", _id);
+        startActivity (intent);
+        finish();
+        super.onBackPressed();
     }
 }
--- app/src/main/java/com/example/renat/projetofinal/VerFornecedor.java
+++ app/src/main/java/com/example/renat/projetofinal/VerFornecedor.java
@@ -1,14 +1,105 @@
 package com.example.renat.projetofinal;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class VerFornecedor extends Activity {
+
+    protected Intent oIntent;
+    protected Button editar;
+    protected Button apagar;
+    protected TextView nome;
+    protected TextView morada;
+    protected TextView contacto;
+    protected TextView email;
+    protected TextView empresa;
+    protected TextView notas;
+    protected AdaptadorBaseDados adb;
+    protected int _id;
+    protected Cursor cursor;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adb = new AdaptadorBaseDados(this).open();
+    }
+    protected void onPause() {
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adb.close();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_fornecedor);
+
+        nome = (TextView) findViewById(R.id.nome);
+        morada = (TextView) findViewById(R.id.morada);
+        contacto = (TextView) findViewById(R.id.contacto);
+        email = (TextView)findViewById(R.id.email);
+        empresa = (TextView) findViewById(R.id.empresa);
+        notas = (TextView) findViewById(R.id.notas);
+
+        adb = new AdaptadorBaseDados(this).open();
+
+        oIntent = getIntent();
+        _id = oIntent.getExtras().getInt("_id");
+        cursor = adb.obterFornecedorId(_id);
+
+        if (cursor.moveToFirst()) {
+            nome.setText(cursor.getString(1));
+            morada.setText(cursor.getString(2));
+            contacto.setText(cursor.getString(3));
+            email.setText(cursor.getString(4));
+            empresa.setText(cursor.getString(5));
+            notas.setText(cursor.getString(6));
+        }
+
+
+
+        editar = (Button) findViewById(R.id.editar);
+        editar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent (VerFornecedor.this, EditarFornecedor.class);
+                intent.putExtra("_id", _id);
+                startActivity (intent);
+            }
+        });
+
+        apagar = (Button) findViewById(R.id.apagar);
+        apagar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                adb.apagarFornecedor(_id);
+                Toast toast = Toast.makeText(VerFornecedor.this, "Fornecedor apagado com sucesso!", Toast.LENGTH_LONG);
+                toast.show();
+
+                Intent i=new Intent (VerFornecedor.this,MainActivity.class);
+                startActivity (i);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent=new Intent (VerFornecedor.this,MainActivity.class);
+        startActivity (intent);
+        finish();
+        super.onBackPressed();
     }
 }
+
